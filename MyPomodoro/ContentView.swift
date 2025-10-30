925ca9f5f2d474cf135c9f0f5887f7e692952c80
Leandro de Sousa <ios@leandrodesousa.dev>
2025-10-30 11:16:10 -0300

    feat: first commit

---
+++ MyPomodoro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyPomodoro
+//
+//  Created by Leandro de Sousa on 30/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyPomodoro/MyPomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPomodoroApp.swift
+//  MyPomodoro
+//
+//  Created by Leandro de Sousa on 30/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MyPomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
